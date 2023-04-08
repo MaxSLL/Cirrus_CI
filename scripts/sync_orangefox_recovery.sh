e5102505ae3d2e0f1301e462a7e5068b310e5981
MaxSLL <118125531+MaxSLL@users.noreply.github.com>
2023-04-09 02:02:33 +0530

    Update sync_orangefox_recovery.sh

--- scripts/sync_orangefox_recovery.sh
+++ scripts/sync_orangefox_recovery.sh
@@ -9,9 +9,9 @@ mkdir /root/OrangeFox_sync
 cd /root/OrangeFox_sync
 git clone https://gitlab.com/OrangeFox/sync.git
 cd /root/OrangeFox_sync/sync/
-./orangefox_sync.sh --branch "12.1" --path /root/orangefox
+./orangefox_sync.sh --branch "11.0" --path /root/orangefox
 cd ../..
 cd orangefox
-git clone https://github.com/MaxSLL/orangefox_device_oneplus_avicii.git -b android-12.1 device/oneplus/avicii
+git clone https://github.com/MaxSLL/twrp_215.git -b ofox-11 device/samsung/m21
 
 exit 0
